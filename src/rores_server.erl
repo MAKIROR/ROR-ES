f41a9d6653308dfd48f7ef664101b23c7db6bf9f
MAKIROR <gzanan@gmail.com>
2023-02-16 22:09:36 +0800

    Update

--- src/rores_server.erl
+++ src/rores_server.erl
@@ -1,119 +1,49 @@
 %%%-------------------------------------------------------------------
+%%% @author Makiror
+%%% @doc
+%%%
+%%% @end
+%%% Created : February 2023
 %%%-------------------------------------------------------------------
 
 -module(rores_server).
 
--behaviour(gen_server).
-
 %% API
 -export([
-         start_link/1,
-         start_link/0,
-         get_count/0,
-         stop/0
-         ]).
+        start/1,
+        start/0,
+        acceptor/1
+        ]).
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
-
--define(SERVER, ?MODULE).
--define(DEFAULT_PORT, 1055).
-
--record(state, {port, lsock, request_count = 0}).
-
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, true}]).
+-define(DEFAULT_PORT, 9527).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
+start(Port) ->
+    {ok, Listener} = gen_tcp:listen(Port, ?TCP_OPTIONS),
+    spawn(fun() -> acceptor(Listener) end).
 
-%%--------------------------------------------------------------------
-%% @doc Starts the server.
-%%
-%% @spec start_link(Port::integer()) -> {ok, Pid}
-%% where
-%%  Pid = pid()
-%% @end
-%%--------------------------------------------------------------------
-start_link(Port) ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
+start() ->
+    start(?DEFAULT_PORT).
 
-%% @spec start_link() -> {ok, Pid}
-%% @doc Calls `start_link(Port)' using the default port.
-start_link() ->
-    start_link(?DEFAULT_PORT).
+acceptor(Listener) -> 
+    {ok, Socket} = gen_tcp:accept(Listener),
+    spawn(fun() -> acceptor(Listener) end),
+    handle_client(Socket).
 
-%%--------------------------------------------------------------------
-%% @doc Fetches the number of requests made to this server.
-%% @spec get_count() -> {ok, Count}
-%% where
-%%  Count = integer()
-%% @end
-%%--------------------------------------------------------------------
-get_count() ->
-    gen_server:call(?SERVER, get_count).
-
-%%--------------------------------------------------------------------
-%% @doc Stops the server.
-%% @spec stop() -> ok
-%% @end
-%%--------------------------------------------------------------------
-stop() ->
-    gen_server:cast(?SERVER, stop).
-
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-init([Port]) ->
-    {ok, LSock} = gen_tcp:listen(Port, [{active, true}]),
-    {ok, #state{port = Port, lsock = LSock}, 0}.
-
-handle_call(get_count, _From, State) ->
-    {reply, {ok, State#state.request_count}, State}.
-
-handle_cast(stop, State) ->
-    {stop, normal, State}.
-
-handle_info({tcp, Socket, RawData}, State) ->
-    do_rpc(Socket, RawData),
-    RequestCount = State#state.request_count,
-    {noreply, State#state{request_count = RequestCount + 1}};
-handle_info(timeout, #state{lsock = LSock} = State) ->
-    {ok, _Sock} = gen_tcp:accept(LSock),
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-
-do_rpc(Socket, RawData) ->
-    try
-        {M, F, A} = split_out_mfa(RawData),
-        Result = apply(M, F, A),
-        gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Result]))
-    catch
-        _Class:Err ->
-            gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Err]))
+handle_client(Socket) ->
+    receive
+        {tcp,Socket}->
+            handle_client(Socket);
+        {tcp_quit, Socket} ->
+            io:format("Close the client connection ~n");
+        Msg ->
+            io:format("got a new message: ~p ~n",[Msg]),
+            handle_client(Socket)
     end.
 
-split_out_mfa(RawData) ->
-    MFA = re:replace(RawData, "\r\n$", "", [{return, list}]),
-    {match, [M, F, A]} =
-        re:run(MFA,
-               "(.*):(.*)\s*\\((.*)\s*\\)\s*.\s*$",
-                   [{capture, [1,2,3], list}, ungreedy]),
-    {list_to_atom(M), list_to_atom(F), args_to_terms(A)}.
 
-args_to_terms(RawArgs) ->
-    {ok, Toks, _Line} = erl_scan:string("[" ++ RawArgs ++ "]. ", 1),
-    {ok, Args} = erl_parse:parse_term(Toks),
-    Args.
+
