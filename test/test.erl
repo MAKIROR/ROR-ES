2c6e494de65b550d2ee272eeff5f734e1a1d3606
MAKIROR <gzanan@gmail.com>
2023-03-11 14:00:56 +0800

    update

--- test/test.erl
+++ test/test.erl
@@ -3,23 +3,27 @@
 -export([run/0]).
 
 run() ->
-    {ok, Socket} = gen_tcp:connect({127,0,0,1}, 9527, [binary, {packet, 0}, {active, true}]),
+    {ok, Socket} = gen_tcp:connect({127,0,0,1}, 9527, [binary, {packet, 2}, {active, true}]),
     gen_tcp:send(Socket, "MAKIROR"),
     receive
-        {tcp, Socket, User} ->
-            io:format("~p~n", [User]),
-            gen_tcp:send(tcp, Socket, "A message"),
-            loop(Socket);
-        {error, _} ->
-            gen_tcp:close(Socket)
-    end,
-ok.
+        {tcp, Socket, BinData} ->
+            case binary_to_term(BinData) of
+                {ok, Username} ->
+                    io:format("Hi! ~s~n", [Username]),
+                    loop(Socket);
+                {auth_failed, Reason} ->
+                    io:format("Verification failed: ~p~n", [Reason])
+            end
+    end.
 
 loop(Socket) ->
     receive
-        {close} ->
-            gen_tcp:close(Socket);
-        {message, Msg} ->
-            io:format("~p~n",[Msg]),
-            loop(Socket)
+        {tcp, Socket, BinData} ->
+            case binary_to_term(BinData) of
+                {close} ->
+                    gen_tcp:close(Socket);
+                {msg, Msg} ->
+                    io:format("~ts~n",[Msg]),
+                    loop(Socket)
+            end
     end.
